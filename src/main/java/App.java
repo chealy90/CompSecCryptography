17991494e44728ddc5fd773d53d7a7d2649fa2b1
chealy90 <christopherhealy20@gmail.com>
2024-11-19 15:31:21 +0000

    Menu Working - will display and return a selected choice as int

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,28 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Here");
+        int menuChoice = 0;
+        while (menuChoice != 3){
+            menuChoice = runMenu();
+            System.out.println("You chose: " + menuChoice);
+        }
+    }
+
+    public static int runMenu(){
+        Scanner kb = new Scanner(System.in);
+        System.out.println("--- Please select an option ---");
+        System.out.println("1: Encrypt a file.\n2: Decrypt a file.\n3: Quit.");
+        int option = 0;
+        try {
+            System.out.print("Your choice:");
+            option = kb.nextInt();
+        } catch (InputMismatchException e){
+            System.out.println("--invalid choice--");
+            runMenu();
+        }
+
+        return option;
     }
 }
